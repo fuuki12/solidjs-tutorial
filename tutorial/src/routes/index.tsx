a630bc0e6b15ca2211a117805adb4ffc67bc39cb
fuki_fuki <fuuki1228@icloud.com>
2023-07-25 04:11:42 +0900

    mod

--- tutorial/src/routes/index.tsx
+++ tutorial/src/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <h1>WAN CHAN!</h1>
       <Article />
       <Counter />
       <p>
